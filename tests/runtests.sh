5261d2fb351140fbb2ab6cc53131604e2772263c
Reimo Rebane <reimo.rebane@cyber.ee>
2015-07-13 15:21:21 +0300

    tests/runtests.sh: Updated the test runner script.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -18,54 +18,112 @@
 # For further information, please contact us at sharemind@cyber.ee.
 #
 
-# Point these variables to the root directories of your SecreC
-# standard libary and Sharemind repositories. Without a trailing slash please.
-STDLIB=
-SHAREMIND=
-# Point this to the SecreC compiler
-SCC=
+if [ -z "${SHAREMIND_PATH}" ]; then
+    exit 1
+fi
 
-compile() { # params $1 - filename.sc, $2 - filename.sb
-    $SCC -I $STDLIB/lib -I $STDLIB/tests --input $1 --output $2
-    cp $2 $SHAREMIND/install.debug/bin/miner1/scripts
-    cp $2 $SHAREMIND/install.debug/bin/miner2/scripts
-    cp $2 $SHAREMIND/install.debug/bin/miner3/scripts
+if [ -z "${TMP_PATH}" ]; then
+    TMP_PATH="." # e.g. /tmp
+fi
+
+# readlink on OS X does not behave as on Linux
+# http://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
+if [ `uname -s` = "Darwin" ]; then
+  CWD=`pwd`
+  TARGET_FILE=$0
+
+  cd "`dirname "${TARGET_FILE}"`"
+  TARGET_FILE=`basename "${TARGET_FILE}"`
+
+  # Iterate down a (possible) chain of symlinks
+  while [ -L "${TARGET_FILE}" ]
+  do
+    TARGET_FILE=`readlink "${TARGET_FILE}"`
+    cd "`dirname "${TARGET_FILE}"`"
+    TARGET_FILE=`basename "${TARGET_FILE}"`
+  done
+  unset -v TARGET_FILE
+
+  # Compute the canonicalized name by finding the physical path
+  # for the directory we're in and appending the target file.
+  ABSSP=`pwd -P`
+  cd "$CWD"
+  unset -v CWD
+else
+  ABSS=`readlink -f "$0"`
+  ABSSP=`dirname "$ABSS"`
+  unset -v ABSS
+fi
+
+TEST_PATH="${ABSSP}"
+
+if [ -d "${SHAREMIND_PATH}/lib" ]; then
+  LD_LIBRARY_PATH="${LD_LIBRARY_PATH}${LD_LIBRARY_PATH:+:}${SHAREMIND_PATH}/lib"
+fi
+
+SCC="${SHAREMIND_PATH}/bin/scc"
+STDLIB="${SHAREMIND_PATH}/lib/sharemind/stdlib"
+TEST_RUNNER="${SHAREMIND_PATH}/bin/SecreCTestRunner"
+
+compile() {
+    local SC="$1"
+    local SB="$2"
+
+    LD_LIBRARY_PATH="${LD_LIBRARY_PATH}" "${SCC}" \
+        --include "${TEST_PATH}" --include "${STDLIB}" \
+        --input "${SC}" --output "${SB}"
 }
 
-runTest() { # param $1 - filename.sc
-    source=$1
-    bytecode=${1%.sc}.sb
+install() {
+    local SB="$1"
+    local SB_BN=`basename "${SB}"`
 
-    compile $source $bytecode
-
-    cd $SHAREMIND/install.debug/bin
-    ./SecreCTestRunner $(basename $bytecode)
-}
-
-runAllTests() {
-    subdirs=$(find $(pwd) -mindepth 1 -type d)
-    for dir in $subdirs
-    do
-        echo $(basename $dir)
-        files=$(find $dir -type f -name "*.sc")
-        for file in $files
-        do
-            runTest $file
-        done
-        echo ""
+    for i in `seq 1 3`; do
+        local SCRIPTS_PATH="${SHAREMIND_PATH}/bin/miner${i}/scripts"
+        mkdir -p "${SCRIPTS_PATH}"
+        cp "${SB}" "${SCRIPTS_PATH}/${SB_BN}"
+        local RV=$?; if [ ${RV} -ne 0 ]; then return ${RV}; fi
     done
 }
 
-if [ "$1" = "" ]
-then
-    wait $!
-    runAllTests
-elif [ -f $1 ]
-then
-    wait $!
-    runTest $1
-else # help
-    echo "Usage of runtests.sh:"
+run() {
+    local SC="$1"
+    local SC_BN=`basename "${SC}"`
+    local SB_BN=`echo "${SC_BN}" | sed 's/\.sc$//' | sed 's/$/.sb/'`
+    local SB="${TMP_PATH}/${SB_BN}"
+
+    echo -n "[`basename "${SC_BN}"`]: "
+
+    compile "${SC}" "${SB}"
+    local RV=$?; if [ ${RV} -ne 0 ]; then return ${RV}; fi
+
+    install "${SB}"
+    local RV=$?; if [ ${RV} -ne 0 ]; then return ${RV}; fi
+
+    local CWD=`pwd`; cd "`dirname "${TEST_RUNNER}"`"
+    LD_LIBRARY_PATH="${LD_LIBRARY_PATH}" "${TEST_RUNNER}" --file "${SB_BN}"
+    local RV=$?; cd "${CWD}"; return ${RV}
+}
+
+run_all() {
+    local RV=0
+    for TESTS in `find "${ABSSP}" -mindepth 1 -maxdepth 1 -type d | sort`; do
+        local TESTS_BN=`basename "${TESTS}"`
+        echo "Testset: ${TESTS_BN}"
+        for TEST in `find "${TESTS}" -mindepth 1 -maxdepth 1 -type f -name "*.sc" | sort`; do
+            run "${TEST}"
+            local RV=$[ $RV || $? ]
+        done
+    done
+    return ${RV}
+}
+
+if [ "x$1" = "x" ]; then
+    run_all
+elif [ -f "$1" ]; then
+    run "$1"
+else
+    echo "Usage of `basename "$0"`:"
     echo "runtests.sh [filename.sc]"
     echo "If no filename is specified, all tests will be run."
 fi
