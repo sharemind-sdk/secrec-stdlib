5cb3d930515beb65c7b441d535b214f5f815dbe8
Ville Sokk <ville.sokk@cyber.ee>
2018-03-28 16:02:11 +0300

    Two fixes to version.sh. When there are multiple release tags pointing to HEAD (eg 2018.03 and 2018.06) take the last one. When adding git hash use the shorter hash.

--- version.sh
+++ version.sh
@@ -8,14 +8,14 @@ res=''
 
 if [ -x "$(command -v git)" ]; then
     releases=$(git tag -l --points-at HEAD | grep release | sed -e 's/release_//' | sort)
-    final=$(echo $releases | tr ' ' '\n' | grep -v rc)
+    final=$(echo $releases | tr ' ' '\n' | grep -v rc | tail -1)
     rc=$(echo $releases | tr ' ' '\n' | grep rc | tail -1)
     if ! [ "$final" = '' ]; then
         res="$version ($final)"
     elif ! [ "$rc" = '' ]; then
         res="$version ($rc)"
     else
-        commit=$(git rev-parse HEAD)
+        commit=$(git rev-parse --short HEAD)
         res="$version (git-$commit)"
     fi
 else
