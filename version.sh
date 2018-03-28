d5cd4bc1a51bdc0ade496f15442d6dca1680e23c
Ville Sokk <ville.sokk@cyber.ee>
2018-03-28 15:42:51 +0300

    Updated the logic that determines the version string.
    We use CMake's PROJECT_VERSION and append the name of the release tag to it in parentheses. So the version is something like "v2.1.0 (2018.03)". If there's no release tag we add the hash of HEAD, eg "v2.1.0 (git-eae4c050cde98fc386dfef0586b43738ceba2d60)". If you don't have git installed then it's "v2.1.0".

---
+++ version.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Determines the year.month tag or git hash that is appended to the
+# version number
+
+version="$1"
+res=''
+
+if [ -x "$(command -v git)" ]; then
+    releases=$(git tag -l --points-at HEAD | grep release | sed -e 's/release_//' | sort)
+    final=$(echo $releases | tr ' ' '\n' | grep -v rc)
+    rc=$(echo $releases | tr ' ' '\n' | grep rc | tail -1)
+    if ! [ "$final" = '' ]; then
+        res="$version ($final)"
+    elif ! [ "$rc" = '' ]; then
+        res="$version ($rc)"
+    else
+        commit=$(git rev-parse HEAD)
+        res="$version (git-$commit)"
+    fi
+else
+    res="$version"
+fi
+
+echo $res
+
