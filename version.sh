a37f53e68bd679468407c7572ccb07fbdd959472
Ville Sokk <ville.sokk@cyber.ee>
2019-01-08 17:01:50 +0200

    version.sh: improved the logic that determines release version from git tag. It didn't work well with the upcoming 2018.12.1 release.

--- version.sh
+++ version.sh
@@ -7,9 +7,11 @@ version="$1"
 res=''
 
 if [ -x "$(command -v git)" ]; then
-    releases=$(git tag -l --points-at HEAD | grep release | sed -e 's/release_//' | sort)
-    final=$(echo $releases | tr ' ' '\n' | grep -v rc | tail -1)
-    rc=$(echo $releases | tr ' ' '\n' | grep rc | tail -1)
+    releases=$(git tag -l --points-at HEAD | grep release | sed -e 's/release_//')
+    final=$(echo $releases | tr ' ' '\n' | grep -v rc | sort | tail -1)
+    # temporarily replace _rc with space because . sorts before _
+    # which would cause a.b_rc1 to be sorted before a_rc1
+    rc=$(echo $releases | tr ' ' '\n' | grep rc | sed -e 's/_rc\([0-9]*\)/ \1/' | sort | sed -e 's/ \([0-9]*\)/_rc\1/' | tail -1)
     if ! [ "$final" = '' ]; then
         res="$version ($final)"
     elif ! [ "$rc" = '' ]; then
