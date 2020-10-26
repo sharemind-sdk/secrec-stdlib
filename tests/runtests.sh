6a4b58698aa88c5e7f020936a192fff6e82421ea
Jaak Ristioja <jaak.ristioja@cyber.ee>
2020-10-26 17:30:08 +0200

    tests/runtests.sh: Simplified ABSSP logic

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -28,34 +28,30 @@
 set -o pipefail
 set -e
 
-# readlink on OS X does not behave as on Linux
-# http://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
-if [ `uname -s` = "Darwin" ]; then
-    CWD=`pwd`
-    TARGET_FILE=$0
+ABSSP=$(
+    # readlink on OS X does not behave as on Linux
+    # http://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
+    if [ "$(uname -s)" = "Darwin" ]; then
+        TARGET_FILE=$0
+        (
+            cd "$(dirname "${TARGET_FILE}")"
+            TARGET_FILE=$(basename "${TARGET_FILE}")
 
-    cd "`dirname "${TARGET_FILE}"`"
-    TARGET_FILE=`basename "${TARGET_FILE}"`
+            # Iterate down a (possible) chain of symlinks
+            while [ -L "${TARGET_FILE}" ]; do
+                TARGET_FILE=$(readlink "${TARGET_FILE}")
+                cd "$(dirname "${TARGET_FILE}")"
+                TARGET_FILE=$(basename "${TARGET_FILE}")
+            done
 
-    # Iterate down a (possible) chain of symlinks
-    while [ -L "${TARGET_FILE}" ]
-    do
-        TARGET_FILE=`readlink "${TARGET_FILE}"`
-        cd "`dirname "${TARGET_FILE}"`"
-        TARGET_FILE=`basename "${TARGET_FILE}"`
-    done
-    unset -v TARGET_FILE
-
-    # Compute the canonicalized name by finding the physical path
-    # for the directory we're in and appending the target file.
-    ABSSP=`pwd -P`
-    cd "$CWD"
-    unset -v CWD
-else
-    ABSS=`readlink -f "$0"`
-    ABSSP=`dirname "${ABSS}"`
-    unset -v ABSS
-fi
+            # Compute the canonicalized name by finding the physical path
+            # for the directory we're in and appending the target file.
+            pwd -P
+        )
+    else
+        dirname "$(readlink -f "$0")"
+    fi
+)
 
 SHAREMIND_PATH=$(cd "${SHAREMIND_PATH:-${ABSSP}/..}"; pwd)
 
